6d714a3886faf8c0a49f715ab538a4526f6e940b
marabakumova <abakumova.ma.le@gmail.com>
2023-02-25 16:28:20 +0300

    Первый коммит

---
+++ test_google_should_find_selene.py
@@ -0,0 +1,20 @@
+import pytest
+from selene import be, have
+from selene.support.shared import browser
+
+
+@pytest.fixture
+def configured_browser():
+    browser.config.window_height = 980
+    browser.config.window_width = 670
+    return browser
+
+
+@pytest.fixture
+def define_url():
+    browser.open('https://google.com')
+
+
+def test_google_research(configured_browser, define_url):
+    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+    browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
