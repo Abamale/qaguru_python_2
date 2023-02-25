5f1fc60f5cad95ac92afac992dbda2fbdbae1ef0
marabakumova <abakumova.ma.le@gmail.com>
2023-02-25 17:08:55 +0300

    Добавила вторую ветку и новый тест

--- test_google_should_find_selene.py
+++ test_google_should_find_selene.py
@@ -18,3 +18,7 @@ def define_url():
 def test_google_research(configured_browser, define_url):
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
+
+def test_for_conflict():
+    assert 3 > 1
